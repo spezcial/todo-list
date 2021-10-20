fa1394dca8604e6a60806b809a8dcf278478e3f9
Георгий Трондин <gtrondin@MacBook-Air.local>
2021-10-20 09:56:11 +0300

    added UPDATE task functionality

--- base/urls.py
+++ base/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import TaskDetail, TaskList, TastCreate
+from .views import TaskDetail, TaskList, TastCreate, TaskUpdate
 
 urlpatterns = [
     path('', TaskList.as_view(), name='tasks'),
     path('task/<int:pk>', TaskDetail.as_view(), name='task'),
     path('task-create/', TastCreate.as_view(), name='task-create'),
+    path('task-update/<int:pk>', TaskUpdate.as_view(), name='task-update'),
 ]
