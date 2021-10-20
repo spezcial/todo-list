4217630dcbd97f33b36639ace237b6bd36665ddc
Георгий Трондин <gtrondin@MacBook-Air.local>
2021-10-20 17:16:57 +0300

    added USER REGISTRATION functionality

--- base/urls.py
+++ base/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
-from .views import TaskDetail, TaskList, TastCreate, TaskUpdate, DeleteView, CustomLoginView
+from .views import RegisterPage, TaskDetail, TaskList, TaskCreate, TaskUpdate, DeleteView, CustomLoginView
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('login/', CustomLoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
+    path('register/', RegisterPage.as_view(), name='register'),
     path('', TaskList.as_view(), name='tasks'),
     path('task/<int:pk>', TaskDetail.as_view(), name='task'),
-    path('task-create/', TastCreate.as_view(), name='task-create'),
+    path('task-create/', TaskCreate.as_view(), name='task-create'),
     path('task-update/<int:pk>', TaskUpdate.as_view(), name='task-update'),
     path('task-delete/<int:pk>', DeleteView.as_view(), name='task-delete'),
 ]
--- base/views.py
+++ base/views.py
@@ -1,13 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
 from django.urls import reverse_lazy
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 from .models import Task
 
-class CustomLoginView(LoginRequiredMixin, LoginView):
+class CustomLoginView(LoginView):
     template_name = 'base/login.html'
     fields = '__all__'
     redirect_authenticated_user = True
@@ -15,7 +17,24 @@ class CustomLoginView(LoginRequiredMixin, LoginView):
     def get_success_url(self):
         return reverse_lazy('tasks')
 
-class TaskList(LoginRequiredMixin ,ListView):
+class RegisterPage(FormView):
+    template_name = 'base/register.html'
+    form_class = UserCreationForm
+    redirect_authenticated_user = True
+    success_url = reverse_lazy('tasks')
+
+def form_valid(self, form):
+    user = form.save()
+    if user is not None:
+        login(self.request, user)
+    return super(RegisterPage, self).form_valid(form)
+
+def get(self, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('tasks')
+        return super(RegisterPage, self).get(*args, **kwargs)
+
+class TaskList(LoginRequiredMixin, ListView):
     model = Task
     context_object_name = 'tasks'
 
@@ -30,14 +49,14 @@ class TaskDetail(LoginRequiredMixin, DetailView):
     context_object_name = 'task'
     template_name = 'base/task.html'
 
-class TastCreate(LoginRequiredMixin, CreateView):
+class TaskCreate(LoginRequiredMixin, CreateView):
     model = Task
     fields = ['title', 'description', 'complete']
     success_url = reverse_lazy('tasks')
 
     def form_valid(self, form):
         form.instance.user = self.request.user
-        return super(TastCreate, self).form_valid(form)
+        return super(TaskCreate, self).form_valid(form)
 
 class TaskUpdate(LoginRequiredMixin, UpdateView):
     model = Task
