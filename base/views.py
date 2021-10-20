d3a58d368f64a2e3bc4fe81688e74853b55f4803
Георгий Трондин <gtrondin@MacBook-Air.local>
2021-10-20 15:28:46 +0300

    added user specific data permission

--- base/views.py
+++ base/views.py
@@ -7,7 +7,7 @@ from django.contrib.auth.views import LoginView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Task
 
-class CustomLoginView(LoginView):
+class CustomLoginView(LoginRequiredMixin, LoginView):
     template_name = 'base/login.html'
     fields = '__all__'
     redirect_authenticated_user = True
@@ -19,22 +19,32 @@ class TaskList(LoginRequiredMixin ,ListView):
     model = Task
     context_object_name = 'tasks'
 
-class TaskDetail(DetailView):
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tasks'] = context['tasks'].filter(user=self.request.user)
+        context['count'] = context['tasks'].filter(complete=False).count()
+        return context
+
+class TaskDetail(LoginRequiredMixin, DetailView):
     model = Task
     context_object_name = 'task'
     template_name = 'base/task.html'
 
-class TastCreate(CreateView):
+class TastCreate(LoginRequiredMixin, CreateView):
+    model = Task
+    fields = ['title', 'description', 'complete']
+    success_url = reverse_lazy('tasks')
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(TastCreate, self).form_valid(form)
+
+class TaskUpdate(LoginRequiredMixin, UpdateView):
     model = Task
     fields = '__all__'
     success_url = reverse_lazy('tasks')
 
-class TaskUpdate(UpdateView):
-    model = Task
-    fields = '__all__'
-    success_url = reverse_lazy('tasks')
-
-class DeleteView(DeleteView):
+class DeleteView(LoginRequiredMixin, DeleteView):
     model = Task
     context_object_name = 'task'
     success_url = reverse_lazy('tasks')
